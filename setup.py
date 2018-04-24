e006fc3277ee369b5c41074a512f276eb662078d
livinginformation <needmoney90@gmail.com>
2018-04-23 20:45:00 -0700

    fix privacy leak

---
+++ setup.py
@@ -0,0 +1,64 @@
+import json
+import os.path
+
+config = {}
+
+
+def menu():
+    while True:
+
+        print("Please make a selection:")
+        print("1. Load existing config")
+        print("2. Create new config")
+        print("3. Exit")
+
+        selection = raw_input("Selection: ")
+
+        if selection =='1':
+            # Load existing config
+            pass
+
+        elif selection == '2':
+            # Create new config
+
+            filename = raw_input("Config file name? ")
+
+            if os.path.isfile("./"+filename):
+                print("Config already exists.")
+                continue
+
+            print("What exchange is this config file for?")
+            print("1. TuxExchange")
+            print("2. Poloniex")
+            print("3. Bittrex")
+            exchange = raw_input("Selection: ")
+
+            if selection == '1':
+                config['exchange'] = 'tux'
+
+                print("API Credentials needed")
+                public_key  = raw_input("Public Key: ")
+                private_key = raw_input("Private Key: ")
+                config['publickey'] = public_key
+                config['privatekey'] = private_key
+
+                with open("./"+filename) # TODO FIX THIS
+
+            elif selection == '2':
+                print("Currently Unsupported")
+                continue
+
+            elif selection == '3':
+                print("Currently Unsupported")
+                continue
+
+            else:
+               print("Unrecognized Selection")
+               continue
+
+        elif selection == '3':
+            # Exit
+            break
+
+        else:
+            print("Unrecognized input.")
