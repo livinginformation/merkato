850cb1eee3f699fd30f528dec4cd3f0e0116591a
livinginformation <>
2018-06-14 16:14:18 -0700

    Remove references to pycrypto
    Re-stub decrypt and encrypt

--- merkato/utils/__init__.py
+++ merkato/utils/__init__.py
@@ -4,32 +4,14 @@ from merkato.exchanges.tux_exchange.exchange import TuxExchange
 from merkato.constants import known_exchanges
 from merkato.utils.database_utils import get_exchange as get_exchange_from_db, get_merkatos_by_exchange, get_merkato
 import base64
-from Crypto.Cipher import AES
-from Crypto.Hash import SHA256
-from Crypto import Random
 
 
 def encrypt(key, source, encode=True):
-    key = SHA256.new(key).digest()  # use SHA-256 over our key to get a proper-sized AES key
-    IV = Random.new().read(AES.block_size)  # generate IV
-    encryptor = AES.new(key, AES.MODE_CBC, IV)
-    padding = AES.block_size - len(source) % AES.block_size  # calculate needed padding
-    source += bytes([padding]) * padding  # Python 2.x: source += chr(padding) * padding
-    data = IV + encryptor.encrypt(source)  # store the IV at the beginning and encrypt
-    return base64.b64encode(data).decode("latin-1") if encode else data
+    pass
 
 
 def decrypt(key, source, decode=True):
-    if decode:
-        source = base64.b64decode(source.encode("latin-1"))
-    key = SHA256.new(key).digest()  # use SHA-256 over our key to get a proper-sized AES key
-    IV = source[:AES.block_size]  # extract the IV from the beginning
-    decryptor = AES.new(key, AES.MODE_CBC, IV)
-    data = decryptor.decrypt(source[AES.block_size:])  # decrypt
-    padding = data[-1]  # pick the padding value from the end; Python 2.x: ord(data[-1])
-    if data[-padding:] != bytes([padding]) * padding:  # Python 2.x: chr(padding) * padding
-        raise ValueError("Invalid padding...")
-    return data[:-padding]  # remove the padding
+    pass
 
 
 def update_config_with_credentials(config):
