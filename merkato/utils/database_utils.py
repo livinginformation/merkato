950e4c2507dd639c9ec9e644ac2a36c0bb1fdedd
livinginformation <>
2018-06-18 23:30:06 -0700

    Add cosmetic changes, stub out merkato kill
    Spacing and stubbing for function comments, as well as stubbing a new function merkato.kill()

--- merkato/utils/database_utils.py
+++ merkato/utils/database_utils.py
@@ -3,6 +3,8 @@ import sqlite3
 from pprint import pprint
 
 def create_merkatos_table():
+    ''' TODO: Function Comment
+    '''
     try:
         conn = sqlite3.connect('merkato.db')
 
@@ -19,24 +21,33 @@ def create_merkatos_table():
 
 
 def no_merkatos_table_exists():
+    ''' TODO: Function Comment
+    '''
     try:
         conn = sqlite3.connect('merkato.db')
+
     except Exception as e:
         print(str(e))
+
     finally:
         c = conn.cursor()
         c.execute('''SELECT count(*) FROM sqlite_master WHERE type="table" AND name="merkatos"''')
         number_of_mutex_tables = c.fetchall()[0][0]
         conn.commit()
         conn.close()
+
         return number_of_mutex_tables == 0
 
 
 def insert_merkato(exchange, exchange_pair='tuxBTC_ETH', base='BTC', alt='XMR', spread='.1', bid_reserved_balance=0, ask_reserved_balance=0, first_order='', profit_limit=10, last_order='', profit_margin=0):
+    ''' TODO: Function Comment
+    '''
     try:
         conn = sqlite3.connect('merkato.db')
+
     except Exception as e:
         print(str(e))
+
     finally:
         c = conn.cursor()
         c.execute("""REPLACE INTO merkatos 
@@ -47,10 +58,14 @@ def insert_merkato(exchange, exchange_pair='tuxBTC_ETH', base='BTC', alt='XMR', 
 
 
 def update_merkato(exchange_pair, key, value):
+    ''' TODO: Function Comment
+    '''
     try:
         conn = sqlite3.connect('merkato.db')
+
     except Exception as e:
         print(str(e))
+
     finally:
         c = conn.cursor()
         query = "UPDATE merkatos SET {} = ? WHERE exchange_pair = ?".format(key)
@@ -59,25 +74,41 @@ def update_merkato(exchange_pair, key, value):
         conn.close()
 
 
+def kill_merkato(exchange_pair):
+    ''' Remove all references to the merkato in the database.
+    '''
+    # TODO
+    pass
+
+
 def get_all_merkatos():
+    ''' TODO: Function Comment
+    '''
     try:
         conn = sqlite3.connect('merkato.db')
+
     except Exception as e:
         print(str(e))
+
     finally:
         c = conn.cursor()
         c.execute("SELECT * FROM merkatos")
         all_merkatos = c.fetchall()
         conn.commit()
         conn.close()
+
         return all_merkatos
 
 
 def create_exchanges_table():
+    ''' TODO: Function Comment
+    '''
     try:
         conn = sqlite3.connect('merkato.db')
+
     except Exception as e:
         print(str(e))
+
     finally:
         c = conn.cursor()
         c.execute('''CREATE TABLE IF NOT EXISTS exchanges
@@ -88,10 +119,14 @@ def create_exchanges_table():
 
 
 def insert_exchange(exchange, public_api_key='', private_api_key='', limit_only=True):
+    ''' TODO: Function Comment
+    '''
     try:
         conn = sqlite3.connect('merkato.db')
+
     except Exception as e:
         print(str(e))
+
     finally:
         c = conn.cursor()
         c.execute("""REPLACE INTO exchanges 
@@ -102,10 +137,14 @@ def insert_exchange(exchange, public_api_key='', private_api_key='', limit_only=
 
 
 def update_exchange(exchange, key, value):
+    ''' TODO: Function Comment
+    '''
     try:
         conn = sqlite3.connect('merkato.db')
+
     except Exception as e:
         print(str(e))
+
     finally:
         c = conn.cursor()
         query = "UPDATE exchanges SET {} = ? WHERE exchange = ?".format(key)
@@ -115,11 +154,15 @@ def update_exchange(exchange, key, value):
 
 
 def get_all_exchanges():
+    ''' TODO: Function Comment
+    '''
     try:
         conn = sqlite3.connect('merkato.db')
         conn.row_factory = sqlite3.Row
+
     except Exception as e:
         print(str(e))
+
     finally:
         c = conn.cursor()
         c.execute("SELECT * FROM exchanges")
@@ -129,29 +172,39 @@ def get_all_exchanges():
         exchange_index = {config["exchange"]:dict(config) for config in all_exchanges}
         exchange_menu = [name for name,config in exchange_index.items()]
         pprint(exchange_index)
+
         return exchange_menu, exchange_index
 
 
 def get_exchange(exchange):
+    ''' TODO: Function Comment
+    '''
     try:
         conn = sqlite3.connect('merkato.db')
         conn.row_factory = dict_factory
+
     except Exception as e:
         print(str(e))
+
     finally:
         c = conn.cursor()
         c.execute('''SELECT * FROM exchanges WHERE exchange = ?''', (exchange,))
         exchange = c.fetchall()[0]
         conn.commit()
         conn.close()
+
         return exchange
 
 
 def exchange_exists(exchange):
+    ''' TODO: Function Comment
+    '''
     try:
         conn = sqlite3.connect('merkato.db')
+
     except Exception as e:
         print(str(e))
+
     finally:
         c = conn.cursor()
         c.execute('''SELECT * FROM exchanges WHERE exchange = ?''', (exchange,))
@@ -159,68 +212,93 @@ def exchange_exists(exchange):
 
         conn.commit()
         conn.close()
+
         return result > 0
 
 
 def merkato_exists(UUID):
+    ''' TODO: Function Comment
+    '''
     try:
         conn = sqlite3.connect('merkato.db')
+
     except Exception as e:
         print(str(e))
+
     finally:
         c = conn.cursor()
         c.execute('''SELECT * FROM merkatos WHERE exchange_pair = ?''', (UUID,))
         result = len(c.fetchall())
         conn.commit()
         conn.close()
+
         return result > 0
 
 
 def no_exchanges_table_exists():
+    ''' TODO: Function Comment
+    '''
     try:
         conn = sqlite3.connect('merkato.db')
+
     except Exception as e:
         print(str(e))
+
     finally:
         c = conn.cursor()
         c.execute('''SELECT count(*) FROM sqlite_master WHERE type="table" AND name="exchanges"''')
         number_of_exchange_tables = c.fetchall()[0][0]
         conn.commit()
         conn.close()
+
         return number_of_exchange_tables == 0
 
 
 def get_merkato(exchange_name_pair):
+    ''' TODO: Function Comment
+    '''
     try:
         conn = sqlite3.connect('merkato.db')
+
     except Exception as e:
         print(str(e))
+
     finally:
         c = conn.cursor()
         c.execute('''SELECT * FROM merkatos WHERE exchange_pair = ?''', (exchange_name_pair,))
         exchange = c.fetchall()[0]
         conn.commit()
         conn.close()
+
         return exchange
 
 
 def get_merkatos_by_exchange(exchange):
+    ''' TODO: Function Comment
+    '''
     try:
         conn = sqlite3.connect('merkato.db')
         conn.row_factory = dict_factory
+
     except Exception as e:
         print(str(e))
+
     finally:
         c = conn.cursor()
         c.execute('''SELECT * FROM merkatos WHERE exchange = ?''', (exchange,))
         exchanges = c.fetchall()
         conn.commit()
         conn.close()
+
         return exchanges
 
 
 def dict_factory(cursor, row):
+    ''' TODO: Function Comment
+    '''
     d = {}
+
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
+
     return d
