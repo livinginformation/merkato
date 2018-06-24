b03dbab50fe16f9d35e060676e889ae92c970e90
Evan Crook <evannc@gmail.com>
2018-06-23 18:38:32 -0700

    Add one test for merkato to prove it can instantiate correctly w logging

--- tests/test_merkato.py
+++ tests/test_merkato.py
@@ -1,15 +1,14 @@
 import unittest
-from mock import patch
+from mock import patch, MagicMock
 
 from merkato.merkato import Merkato
-from merkato.exchanges.test_exchange.exchange import TestExchange
 
 
 class MerkatoTestCase(unittest.TestCase):
 	@patch('merkato.merkato.merkato_exists', return_value=True)
 	@patch('merkato.merkato.get_first_order', return_value=None)
 	@patch('merkato.merkato.get_last_order', return_value=None)
-	@patch('merkato.merkato.get_relevant_exchange', return_value=TestExchange)
+	@patch('merkato.merkato.get_relevant_exchange', return_value=MagicMock())
 	def setUp(self, *_):
 		config = {"exchange": "tux", "private_api_key": "abc123", "public_api_key": "def456", "limit_only": False}
 		self.merkato = Merkato(config, coin='XMR', base='BTC', spread='.1',
@@ -17,6 +16,9 @@ class MerkatoTestCase(unittest.TestCase):
 
 	def test_rebalance_orders__no_new_txes(self):
 		result = self.merkato.rebalance_orders([])
+		self.assertEqual(result, [])
+		self.merkato.exchange.buy.assert_not_called()
+		self.merkato.exchange.sell.assert_not_called()
 
 	def test_rebalance_orders(self):
 		pass
